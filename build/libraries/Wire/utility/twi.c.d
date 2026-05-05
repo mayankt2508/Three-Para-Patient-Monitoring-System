219e1d0726834f54824b9591ead767fdc41eff5f
Mayank Thakkar <139612749+mayankt2508@users.noreply.github.com>
2026-05-05 20:36:52 +0530

    Add files via upload

---
+++ build/libraries/Wire/utility/twi.c.d
@@ -0,0 +1,6 @@
+c:\Users\aryan\Documents\All\ Projects\Academic\PICT\PBL\build\libraries\Wire\utility\twi.c.o: \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.c \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard/pins_arduino.h \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.h
