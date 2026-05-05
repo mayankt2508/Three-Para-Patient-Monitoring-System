c424ea78e83fa113d7cdbaef1e467c9e5b839ad2
Mayank Thakkar <139612749+mayankt2508@users.noreply.github.com>
2026-05-05 20:35:34 +0530

    Add files via upload

---
+++ build/libraries/Wire/Wire.cpp.d
@@ -0,0 +1,8 @@
+c:\Users\aryan\Documents\All\ Projects\Academic\PICT\PBL\build\libraries\Wire\Wire.cpp.o: \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.cpp \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility/twi.h \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.h \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
+ C:\Users\aryan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h
